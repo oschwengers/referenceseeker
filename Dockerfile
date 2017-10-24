6c8c8a1acfa475ba013393a5f59e738b37167f25
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2017-10-24 15:57:03 +0200

    enhance and fix Docker version

--- Dockerfile
+++ Dockerfile
@@ -10,12 +10,12 @@ RUN apt-get -y update && apt-get -y install \
     python3-pip
 RUN pip3 install biopython
 
-COPY refseekr /
+COPY referenceseeker.py /
 COPY share/ /share
 
 ENV REFERENCE_SEEKER_HOME=/
 
 WORKDIR /data
 
-ENTRYPOINT [ "python3", "/refseekr.py", "--db", "/db" ]
+ENTRYPOINT [ "python3", "/referenceseeker.py", "--db", "/db" ]
 CMD ["--help"]
---
+++ referenceseeker.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo 1>&2 "Usage: $0 <db-path> <fasta-file>"
+  exit 3
+fi
+
+set -e
+
+if [ ! -d $1 ]
+  then
+	echo "database directory does not exist!"
+	exit 1
+  else
+	db="$(dirname $(readlink -e $1))/$(basename $1)"
+fi
+
+if [ ! -f $2 ]
+  then
+	echo "fasta file does not exist or is not a file!"
+	exit 1
+  else
+	data="$(dirname $(readlink -e $2))"
+	genome="$(basename $2)"
+fi
+
+echo "db: $db"
+echo "data: $data"
+echo "genome: $genome"
+
+
+sudo docker run \
+	--rm \
+	-v $db:/db \
+	-v $data:/data \
+	referenceseeker \
+	--genome $genome
+
