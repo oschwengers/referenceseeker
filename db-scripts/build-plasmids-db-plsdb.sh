acac0c70d61ea10cfd93404778cb3671a9d5c256
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2024-01-11 09:41:04 +0100

    update PLASDB build script

--- db-scripts/build-plasmids-db-plsdb.sh
+++ db-scripts/build-plasmids-db-plsdb.sh
@@ -1,15 +1,14 @@
 
 echo "Download plasmid sequences..."
-wget -O plasmids-plsdb.zip https://ccb-microbe.cs.uni-saarland.de/plsdb/plasmids/download/?zip
+wget -O plasmids-plsdb.bz2 https://ccb-microbe.cs.uni-saarland.de/plsdb/plasmids/download/plsdb.fna.bz2
 
 echo "Unzip plasmid sequences..."
-unzip plasmids-plsdb.zip
-blastdbcmd -entry all -db plsdb.fna -out plsdb.fna
+bunzip2 plsdb.fna.bz2
 nextflow run $REFERENCE_SEEKER_HOME/db-scripts/build-plasmids-db-plsdb.nf --plasmids plsdb.fna  ||  { echo "Nextflow failed!"; exit; }
 
 mash paste db sketches/*.msh  ||  { echo "Mash failed!"; exit; }
 
-rm -rf work/ .nextflow* sketches/ plsdb* README.md
+rm -rf work/ .nextflow* sketches/ plsdb.fna
 
 mv db.msh plasmids-plsdb/
 
